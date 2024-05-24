aa524f7d4d7a587921097a3d66ee06eff5285b99
vagrant <vagrant@homestead>
2024-05-24 10:28:00 +0800

    注册于登录功能

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,8 +2,8 @@
   <div class="container">
     <!-- Branding Image -->
     <div class="item">
-        <!-- <img class="ui image" style="width:26px;height:26px;margin:-2px 6px 0 0;" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png!/both/44x44"> -->
-        <img class="ui image" style="width:26px;height:26px;margin:-2px 6px 0 0;" src="https://www.zhanzhangl.com/wp-content/uploads/2024/04/cropped-2024041014585673.jpg">
+        <img class="ui image" style="width:26px;height:26px;margin:-2px 6px 0 0;" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png!/both/44x44">
+        <!-- <img class="ui image" style="width:26px;height:26px;margin:-2px 6px 0 0;" src="https://www.zhanzhangl.com/wp-content/uploads/2024/04/cropped-2024041014585673.jpg"> -->
         <a class="navbar-brand " href="{{ url('/') }}">
           Five App
         </a>
@@ -18,13 +18,31 @@
       <ul class="navbar-nav">
         <li class="nav-item"><a class="nav-link" href="#">文档</a></li>
         <li class="nav-item"><a class="nav-link" href="#">会员</a></li>
-      </ul><Script></Script>
+      </ul>
 
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
-        <!-- Authentication Links -->
-        <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+        <!-- 登录注册链接开始 -->
+        @guest
+        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+        @else
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+ aria-haspopup="true" aria-expanded="false">
+            <img src="https://www.zhanzhangl.com/wp-content/uploads/2024/04/cropped-2024041014585673.jpg" class="img-responsive img-circle" width="30px" height="30px">
+            {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" id="logout" href="#"
+               onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+            </form>
+          </div>
+        </li>
+        @endguest
+        <!-- 登录注册链接结束 -->
       </ul>
     </div>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', 'PagesController@root')->name('root');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
