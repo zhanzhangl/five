b10d821ae9ff971cd9ced0720447e54500dfcd04
vagrant <vagrant@homestead>
2024-05-24 14:34:15 +0800

    邮箱认证成功后的友好提示

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,8 @@
     <div id="app" class="{{ route_class() }}-page">
         @include('layouts._header')
         <div class="container">
-            @yield('content')
+        @include('shared._messages')
+        @yield('content')
         </div>
         @include('layouts._footer')
     </div>
